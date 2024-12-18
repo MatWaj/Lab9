74f212598a452eac6f84498e81a0e148581ca0c1
PawelS <58605803+PawelSolecki@users.noreply.github.com>
2024-12-18 14:08:11 +0100

    Update backsubst.c

--- src/backsubst.c
+++ src/backsubst.c
@@ -4,19 +4,34 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+int backsubst(Matrix *x, Matrix *mat, Matrix *b)
+{
+	/**
+	 * Tutaj należy umieścić właściwą implemntację.
+	 */
+	int n = mat->r; // Liczba wierszy (macierz kwadratowa)
+	int i, j;
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+	// Inicjalizacja wyniku w macierzy x
+	for (i = 0; i < n; i++)
+	{
+		x->data[i][0] = 0.0;
+	}
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+	// Algorytm wstecznego podstawienia
+	for (i = n - 1; i >= 0; i--)
+	{
+		double sum = 0.0;
 
-				return 0;
+		// Obliczenie sumy dla części Ax = b
+		for (j = i + 1; j < n; j++)
+		{
+			sum += mat->data[i][j] * x->data[j][0];
+		}
+
+		// Obliczenie wartości niewiadomej x[i]
+		x->data[i][0] = (b->data[i][0] - sum) / mat->data[i][i];
+	}
+
+	return 0; // Operacja zakończona sukcesem
 }
-
-
